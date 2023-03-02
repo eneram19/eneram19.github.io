f8012164b7b2f5fec7c06bc4079ff9e17c20c298
eneram19 <96058522+eneram19@users.noreply.github.com>
2023-03-02 12:45:48 +0100

    Add files via upload

---
+++ med2/javascript/med2/javascript/myScript2.js
@@ -0,0 +1,7 @@
+// Der promt Befehl erzeugt eiene Angabebox
+//promt("Wie alt bist du?");
+
+// Text steht immer unter Ausführungszeichen.
+// Text und Variablen kann man mit Pluszeichen kombinieren
+
+alert( "Du bist "+prompt("Wie alt bist du?")+" Jahre alt!");
